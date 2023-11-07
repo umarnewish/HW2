3eaa5f2bf516b904b116a777d8982b4322fd77c6
Omar <47752009+omar-tashkent@users.noreply.github.com>
2023-11-07 23:26:40 +0500

    First commit

---
+++ main.cpp
@@ -0,0 +1,58 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    //Problem 1
+
+/*    int a, b, c, av, p;
+    cin >> a >> b >> c;
+
+    av = (a + b + c) / 3;
+    cout << "the average is " << av << endl;
+
+    p = a * b * c;
+    cout << "the product is " << p;*/
+
+    //Problem 2
+
+/*    double length, height, area;
+    cin >> length >> height;
+
+    area = 0.5 * height * length;
+    cout << "the are is " << area;*/
+
+    //Problem 3
+
+/*    double celsius, farenheiht;
+    cin >> celsius;
+
+    farenheiht = celsius * 1.8 + 32;
+
+    cout << "The " << celsius << " degrees in Celsius is equal to " << farenheiht << " degrees in Farenheight";*/
+
+    //Problem 4
+
+/*    int a;
+    cin >> a;
+    if (a >= 50) {
+        cout << 50 - a << " computers will stay free in the class";
+    }
+    else{
+        cout << "Ypu entered wrong value";
+    }*/
+
+    // Problem 5
+
+/*    cout << "The size of bool is " << sizeof(bool) << " byte" << endl;
+    cout << "The size of short is " << sizeof(short) << " bytes" << endl;
+    cout << "The size of int is " << sizeof(int) << " bytes" << endl;
+    cout << "The size of char is " << sizeof(char) << " byte" << endl;
+    cout << "The size of long is " << sizeof(long) << " bytes" << endl;
+    cout << "The size of long long is " << sizeof(long long) << " bytes" << endl;
+    cout << "The size of float is " << sizeof(float) << " bytes" << endl;
+    cout << "The size of double is " << sizeof(double) << " bytes" << endl;
+    cout << "The size of long double is " << sizeof(long double) << " bytes" << endl;
+    */
+    return 0;
+}
